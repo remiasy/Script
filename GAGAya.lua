49ae1da8af01f7215cc640cdef590c1d8dc501a4
remiasy <thanawin.business@hotmail.com>
2025-08-26 01:29:37 +0700

    Update GAGAya.lua

--- GAGAya.lua
+++ GAGAya.lua
@@ -14,8 +14,8 @@ getgenv().ConfigsKaitun = {
 	
 	["Rejoin When Update"] = false,
 	["Limit Tree"] = {
-		["Limit"] = 400,
-		["Destroy Untill"] = 400,
+		["Limit"] = 250,
+		["Destroy Untill"] = 200,
 		
 		["Safe Tree"] = {
 			"Moon Blossom",
