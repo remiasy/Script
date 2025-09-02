70dbd2c7f8ef0d97f9754403d9c3f97bf3716e3c
remiasy <thanawin.business@hotmail.com>
2025-09-03 04:29:46 +0700

    Update GAGAya.lua

--- GAGAya.lua
+++ GAGAya.lua
@@ -225,7 +225,7 @@ getgenv().ConfigsKaitun = {
 	
 	Events = {
 		["Fairy Event"] = {
-			Minimum_Money = 10_000_000, -- minimum money to start play this event
+			Minimum_Money = 100000, -- minimum money to start play this event
 			Rewards_Item = { -- top mean select first
 				"Pet Shard Glimmering",
 				"Enchanted Egg",
