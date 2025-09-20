29cf60c01f0484d16311b7e003f3f92a608ea673
remiasy <thanawin.business@hotmail.com>
2025-09-20 22:03:19 +0700

    Update hwidmobile.lua

--- hwidmobile.lua
+++ hwidmobile.lua
@@ -1,5 +1,18 @@
 repeat task.wait() until game:IsLoaded()
 -- ========================================================================================
+task.spawn(function()
+    -- เรียกครั้งแรกทันที
+    setfpscap(10)
+    print("Setfpscap : Done (initial)")
+
+    -- จากนั้นค่อยวนทุก 5 นาที
+    while task.wait(300) do
+        setfpscap(10)
+        print("Setfpscap : Done (loop)")
+    end
+end)
+
+
 function Farm_Script()
 print("Farm");
 local Keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/remiasy/Script/refs/heads/main/banana.txt"))()
@@ -122,7 +135,7 @@ getgenv().HorstConfig = {
     ["Whitescreen"] = false,
     ["EnableAddFriends"] = true, -- แอดเพื่อนให้เอง ทุกๆ 1นาที
     ["LockFps"] = {
-        ["EnableLockFps"] = true,
+        ["EnableLockFps"] = false,
         ["LockFpsAmount"] = 10 
     },
     ["AutoFunctions"] = {
@@ -477,6 +490,34 @@ UserInputService.InputBegan:Connect(function(input,gp)
 end)
 end
 -- ========================================================================================
+function delta_Fix()
+local errorMSG = { -- ปิด Roblox Client เมื่อเจอ error
+    "you were kicked", "disconnected", "lost connection", "267", "279",
+}
+
+task.spawn(function()
+    while task.wait(1) do
+        local promptGui = game.CoreGui:FindFirstChild("RobloxPromptGui")
+        if promptGui then
+            for _, v in ipairs(promptGui:GetDescendants()) do
+                if v:IsA("TextLabel") and v.Text and v.Text ~= "" then
+                    local txt = string.lower(v.Text)
+                    for _, msg in ipairs(errorMSG) do
+                        if string.find(txt, msg, 1, true) then
+                            pcall(function()
+                                game:Shutdown() -- ปิด Roblox Client ทันที
+                            end)
+                            return
+                        end
+                    end
+                end
+            end
+        end
+    end
+end)
+
+end
+-- ========================================================================================
 
 
 repeat task.wait() until game:IsLoaded()
@@ -502,3 +543,5 @@ task.wait(0.1)
 safe_spawn(Autochat_Script, "Autochat_Script")
 task.wait(0.1)
 safe_spawn(Fps_Script, "Fps_Script")
+task.wait(0.1)
+safe_spawn(delta_Fix, "delta_Fix")
