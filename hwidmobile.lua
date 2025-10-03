b256b97d90c182bcc229ac2a23a0210955c2a494
remiasy <thanawin.business@hotmail.com>
2025-10-03 10:42:16 +0700

    Update hwidmobile.lua

--- hwidmobile.lua
+++ hwidmobile.lua
@@ -545,8 +545,8 @@ _G.Races_Settings = {
     }
 }
 _G.Fruits_Settings = {
-    ['Main_Fruits'] = {'Dough-Dough'},
-    ['Select_Fruits'] = {"Flame-Flame", "Ice-Ice", "Quake-Quake", "Light-Light", "Dark-Dark", "Spider-Spider", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha"}
+    ['Main_Fruits'] = {''},
+    ['Select_Fruits'] = {"Ice-Ice", "Light-Light", "Dark-Dark",, "Magma-Magma", "Buddha-Buddha"}
 }
 _G.Settings_Melee = {
     ['Superhuman'] = true,
@@ -619,10 +619,10 @@ safe_spawn(Log_script, "Log_script")
 task.wait(0.1)
 safe_spawn(Api_Script, "Api_Script")   -- ให้ API ขึ้นก่อน
 task.wait(0.1)
-safe_spawn(Farm_Script, "Farm_Script") -- Banana
-task.wait(0.1) -- Banana
---safe_spawn(FarmMaru_Script, "FarmMaru_Script") -- Maru
---task.wait(0.1) -- Maru
+--safe_spawn(Farm_Script, "Farm_Script") -- Banana
+--task.wait(0.1) -- Banana
+safe_spawn(FarmMaru_Script, "FarmMaru_Script") -- Maru
+task.wait(0.1) -- Maru
 safe_spawn(Autochat_Script, "Autochat_Script")
 task.wait(0.1)
 safe_spawn(Fps_Script, "Fps_Script")
