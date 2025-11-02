250b84d43b4a6e9cda5b47ee191b29cbe5a49177
remiasy <thanawin.business@hotmail.com>
2025-11-02 11:19:42 +0700

    Update hwidmobile.lua

--- hwidmobile.lua
+++ hwidmobile.lua
@@ -730,10 +730,10 @@ safe_spawn(Log_script, "Log_script")
 task.wait(0.1)
 safe_spawn(Api_Script, "Api_Script")   -- ให้ API ขึ้นก่อน
 task.wait(0.1)
---safe_spawn(Farm_Script, "Farm_Script") -- Banana
---task.wait(0.1) -- Banana
-safe_spawn(FarmMaru_Script, "FarmMaru_Script") -- Maru
-task.wait(0.1) -- Maru
+safe_spawn(Farm_Script, "Farm_Script") -- Banana
+task.wait(0.1) -- Banana
+--safe_spawn(FarmMaru_Script, "FarmMaru_Script") -- Maru
+--task.wait(0.1) -- Maru
 --safe_spawn(Autochat_Script, "Autochat_Script")
 --task.wait(0.1)
 --safe_spawn(Private_Server_Bloxfruit, "Private_Server_Bloxfruit")
@@ -742,4 +742,3 @@ task.wait(0.1) -- Maru
 --task.wait(0.1)
 --safe_spawn(QuartyZ_Script, "QuartyZ_Script") -- QuartyZ
 --task.wait(0.1) -- QuartyZ
-
