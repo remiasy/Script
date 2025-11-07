7fe133b3950169f2b96638894828d40137e2b0c4
remiasy <thanawin.business@hotmail.com>
2025-11-07 22:54:26 +0700

    Update hwid4.lua

--- hwid4.lua
+++ hwid4.lua
@@ -129,6 +129,7 @@ end
 function Api_Script()
 print("Api");
 repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players.LocalPlayer.Team ~= nil
 getgenv().HorstConfig = {
     ["EnableLog"] = true, -- ปรับเป็น true ถ้าอยากให้มันเช็คของ BF / GAG
     ["Whitescreen"] = false,
