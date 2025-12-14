d86b2163895bb5f16700374864dd63afca192242
remiasy <thanawin.business@hotmail.com>
2025-12-14 12:42:01 +0700

    Update ainz.lua

--- ainz.lua
+++ ainz.lua
@@ -345,8 +345,8 @@ local defaultConfig = {
     },
     
     THRESHOLDS = getgenv().THRESHOLDS or {
-        LOBBY_SUMMON = 1500,
-        SPECIAL_PLACE = 100000
+        LOBBY_SUMMON = 3000000,
+        SPECIAL_PLACE = 3000000
     }
 }
 
@@ -1342,7 +1342,7 @@ local function mainFarmLoop()
     CONFIG.THRESHOLDS.SPECIAL_PLACE = 5000000  -- ฟาร์มถึง 5M
     Utils.log("ESCANOR", "Escanor owned → Farming until 5,000,000 Leaves")
 else
-    CONFIG.THRESHOLDS.SPECIAL_PLACE = 100000   -- ฟาร์มถึง 100k ปกติ
+    CONFIG.THRESHOLDS.SPECIAL_PLACE = 3000000   -- ฟาร์มถึง 100k ปกติ
     Utils.log("ESCANOR", "Escanor not owned → Farming until 100,000 Leaves")
 end
     
