e038e6672d69a01fc7ffb50ef78e28ba5f77443d
remiasy <thanawin.business@hotmail.com>
2026-02-08 01:01:19 +0700

    Update hwid3.lua

--- hwid3.lua
+++ hwid3.lua
@@ -1,7 +1,7 @@
-setfpscap(10)
+setfpscap(6)
 repeat task.wait() until game:IsLoaded()
 task.spawn(function()
-    local FPS = 10
+    local FPS = 8
     while task.wait(60) do
         pcall(setfpscap, FPS)
     end
